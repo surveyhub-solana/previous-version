a70f49d8c44b8738fdea93b247d4c231e252b46b
Le Dai <lexuandaibn@gmail.com>
2024-07-30 01:17:45 +0700

    add meta data

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -39,10 +39,11 @@ export const metadata: Metadata = {
   description:
     'SurveyHub - Nền tảng ứng dụng công nghệ Blockchain vào khảo sát trực tuyến giúp các dữ liệu khảo sát minh bạch và có tính ứng dụng cao. Bên cạnh đó SurveyHub cung cấp các thông tin từ dữ liệu khảo sát tới người dùng thông qua việc tích hợp AI và huấn luyện trên tập dữ liệu Blockchain.',
   openGraph: {
-    images: 'https://edzmate.liamlee.id.vn/image/logo-edzmate.png',
-    title: 'Edzmate',
-    description: 'Edzmate - Trợ giảng hỗ trợ trong giáo dục',
-    url: 'https://edzmate.liamlee.id.vn',
+    images: 'https://surveyhub-eta.vercel.app/favicon.ico',
+    title: 'SurveyHub',
+    description:
+      'SurveyHub - Nền tảng khảo sát trực tuyến ứng dụng công nghệ Blockchain',
+    url: 'https://surveyhub-eta.vercel.app/',
   },
 };
 
