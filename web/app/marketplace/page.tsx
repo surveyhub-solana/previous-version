ca958683af32f544b7e648ba4e115475556fc7bd
Le Dai <lexuandaibn@gmail.com>
2024-09-17 07:37:42 +0700

    remove wrap token

--- web/app/marketplace/page.tsx
+++ web/app/marketplace/page.tsx
@@ -4,7 +4,7 @@ import WormholeConnect from '@wormhole-foundation/wormhole-connect';
 export default function marketplace() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <WormholeConnect />
+      {/* <WormholeConnect /> */}
     </div>
   );
 }
